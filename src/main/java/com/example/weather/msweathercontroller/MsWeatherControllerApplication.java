9064c96ed72164cb729be1f5d5e7b5500dd9561c
Irshad Eyvazov <eyvazovirsad@gmail.com>
2022-10-11 14:58:14 +0400

    init commit

---
+++ src/main/java/com/example/weather/msweathercontroller/MsWeatherControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather.msweathercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWeatherControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsWeatherControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weather/msweathercontroller/MsWeatherControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather.msweathercontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWeatherControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
